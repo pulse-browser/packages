cf312febe5cc26f3729d640b7128670b3bb5f597
TrickyPR <trickypr@icloud.com>
2021-04-06 19:30:13 +1000

    💄 Icons for flatpak

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -4,13 +4,13 @@
 echo "Install runtime and SDK"
 echo "======================="
 # -y assumes yes
-flatpak install flathub org.freedesktop.Platform//19.08 org.freedesktop.Sdk//19.08 -y
+flatpak install flathub org.gnome.Platform//3.38 org.gnome.SDK//3.38 -y
 
 clear
 echo "Downloading latest linux binary"
 echo "==============================="
 # TODO: This needs to be far more dynamic
-wget https://github.com/dothq/browser-desktop/releases/download/87.0-717456043/dot-87.0.tar.bz2
+wget https://github.com/dothq/browser-desktop/releases/download/87.0-720629329/dot-87.0.tar.bz2
 
 clear
 echo "Extract linux binary"
