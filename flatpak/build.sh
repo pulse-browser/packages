e73407cddbe9b49258cd8b1814d69f7d3279438f
trickypr <trickypr@icloud.com>
2022-01-18 16:36:10 +1100

    🚚 Rename to pulse browser

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -2,13 +2,13 @@ echo "Clone the repo..."
 
 # TODO: Create a robot github account and make it responsible for pushing updates to the repo
 rm -rf repo/
-git clone https://trickypr:${deploy_acc_key}@github.com/focus-browser/repo
+git clone https://trickypr:${deploy_acc_key}@github.com/pulse-browser/repo
 mkdir -p repo/flatpak/alpha
 
 echo "Building flatpak for the alpha repo"
 
 rm -rf focus-build
-flatpak-builder --repo=repo/flatpak/alpha/ --gpg-sign=856780A2B7EAC3C2AA34C1DCC210F0A67AAA29F4 ./focus-build ./com.fushra.browser.desktop.yml
+flatpak-builder --repo=repo/flatpak/alpha/ --gpg-sign=856780A2B7EAC3C2AA34C1DCC210F0A67AAA29F4 ./pulse-build ./com.fushra.browser.desktop.yml
 
 echo "Commiting to the repo"
 
